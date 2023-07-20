ced949d8710e4963c012ade51db790de18a39600
George Pavlides <georgepavlideswork@gmail.com>
2023-07-20 09:37:09 +0300

    second commit

--- main.js
+++ main.js
@@ -3,51 +3,18 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { gsap } from "gsap";
+// import {stats} from "stats.js";
 
-/**
- * Base
- */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff); 
+
+// GLTF loader
 const loader = new GLTFLoader();
-
 let gltfModel = null;
-
-const vertexShader = `
-  varying vec3 vUv; 
-
-  void main() {
-    vUv = position; 
-
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
-  }
-`;
-
-const fragmentShader = `
-  uniform float u_time;
-  varying vec3 vUv;
-
-  void main() {
-    vec3 colorA = vec3(1.0, 0.0, 0.0); // red color
-    vec3 colorB = vec3(0.0, 1.0, 0.0); // green color
-    vec3 colorC = vec3(0.0, 0.0, 1.0); // blue color
-
-    // Calculate blend factors based on the time
-    float blendFactor1 = 0.5 * (1.0 + sin(u_time));
-    float blendFactor2 = 0.5 * (1.0 + sin(u_time + 2.0 * 3.14159 / 3.0)); // offset by 120 degrees
-    float blendFactor3 = 0.5 * (1.0 + sin(u_time + 4.0 * 3.14159 / 3.0)); // offset by 240 degrees
-
-    // Interpolate between the colors using the blend factors
-    vec3 color = mix(colorA, colorB, blendFactor1);
-    color = mix(color, colorC, blendFactor2);
-    color = mix(color, colorA, blendFactor3);
-
-    gl_FragColor = vec4(color, 1.0);
-  }
-`;
 
 const uniforms = {
   u_time: { value: 0.0 },
@@ -55,171 +22,37 @@ const uniforms = {
 
 const shaderMaterial = new THREE.ShaderMaterial({
   uniforms: uniforms,
-  vertexShader: vertexShader,
-  fragmentShader: fragmentShader,
+  vertexShader: `
+    varying vec2 vUv;
+
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    }
+  `,
+  fragmentShader: `
+    uniform float u_time;
+    varying vec2 vUv;
+
+    void main() {
+      gl_FragColor = vec4(vUv, sin(u_time), 1.);
+    }
+  `,
 });
-//========================================= CUVE GLTF==================
 
+// Shadow
+const shadowMaterial = new THREE.ShadowMaterial({ opacity: 0.4 });
 
-loader.load(
-  "/cubemain2.glb", // path to your gltf file
-  function (gltf) {
-    gltfModel = gltf.scene;
-    scene.add(gltf.scene);
-
-    const planes = ["Plane1", "Plane2", "Plane3", "Plane4", "Plane5"];
-
-    planes.forEach((planeName) => {
-      const plane = gltfModel.getObjectByName(planeName, true);
-
-      // Apply the shader material to the object if it exists
-      if (plane) {
-        plane.material = shaderMaterial;
-      }
-    });
-
-    // Define a timeline
-
-    //ANIMATIOn
-
-    let tl = gsap.timeline({ repeat: -1, yoyo: true });
-
-    // // Animate to x+ then x-
-    // tl.to(gltfModel.position, { x: 3, duration: 6, ease: "power1.inOut" }).to(
-    //   gltfModel.position, { x: -3, duration: 6, ease: "power1.inOut" }
-    // );
-
-    gsap.to(gltfModel.rotation, { y: "+=" + Math.PI * 2, duration: 16, ease: "none", repeat: -1 });
-  },
-  // called while loading is progressing
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  // called when loading has errors
-  function (error) {
-    console.log("An error happened");
-  }
-);
-
-// ============================= FLOOR GLTF =================================================
-// loader.load(
-//   "/floor.glb", // path to your gltf file
-//   function (gltf) {
-//     gltfModel = gltf.scene;
-//     scene.add(gltf.scene);
-   
-//   },
-//   // called while loading is progressing
-//   function (xhr) {
-//     console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-//   },
-//   // called when loading has errors
-//   function (error) {
-//     console.log("An error happened");
-//   }
-// );
-
-
-
-/**
- * Lights
- */
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2); // soft white light
-scene.add(ambientLight);
-
-// Hemisphere Light
-const hemisphereLight = new THREE.HemisphereLight(0x1fb8b2, 0x0000ff, 0.3);
-scene.add(hemisphereLight);
-
-// Directional Light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(0, 2, 2);
-scene.add(directionalLight);
-
-// Point Light
-const pointLight = new THREE.PointLight(0x00fffc, 0.3);
-pointLight.position.set(2, 3, 4);
-scene.add(pointLight);
-
-// /**
-//  * Object
-//  */
-// const geometry = new THREE.BufferGeometry();
-// const count = 50;
-// const positionsArray = new Float32Array(count * 3 * 3);
-// for (let i = 0; i < count * 3 * 3; i++) {
-//   positionsArray[i] = (Math.random() - 0.5) * 500;
-// }
-// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-// geometry.setAttribute("position", positionsAttribute);
-
-// const material = new THREE.MeshBasicMaterial({
-//   color: 0x1fb8b2,
-//   wireframe: true,
-// });
-
-// const mesh = new THREE.Mesh(geometry, material);
-// scene.add(mesh);
-
-// /**
-//  * New Object
-//  */
-// const geometry2 = new THREE.BufferGeometry();
-// const count2 = 35;
-// const positionsArray2 = new Float32Array(count2 * 3 * 3);
-// for (let i = 0; i < count2 * 3 * 3; i++) {
-//   positionsArray2[i] = (Math.random() - 0.5) * 1000;
-// }
-// const positionsAttribute2 = new THREE.BufferAttribute(positionsArray2, 3);
-// geometry2.setAttribute("position", positionsAttribute2);
-
-// const material2 = new THREE.MeshBasicMaterial({
-//   color: 0xffffff,
-//   wireframe: true,
-// });
-
-// const mesh2 = new THREE.Mesh(geometry2, material2);
-// scene.add(mesh2);
-
-/**
- * Sizes
- */
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 5;
 
 scene.add(camera);
 
-// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.maxPolarAngle = Math.PI / 2 - 0.1;
@@ -229,82 +62,128 @@ controls.enablePan = false;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 2.0;
 
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-/**
- * Raycaster
- */
-// let isScaledUp = false;
-// const raycaster = new THREE.Raycaster();
-// const mouse = new THREE.Vector2();
-
-// function onMouseClick(event) {
-//   // calculate mouse position in normalized device coordinates
-//   // (-1 to +1) for both components
-//   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-//   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-//   raycaster.setFromCamera(mouse, camera);
-
-//   // calculate objects intersecting the picking ray
-//   const intersects = raycaster.intersectObjects(scene.children, true);
-
-//   for (let i = 0; i < intersects.length; i++) {
-//     let targetObject = intersects[i].object;
-
-//     // Recursive search to find if the gltfModel is a parent of the intersected object
-//     while (targetObject !== null) {
-//       if (targetObject === gltfModel) {
-//         console.log("GLTF model clicked");
-
-//         // Use GSAP to animate the scale of the GLTF model
-//         gsap.to(gltfModel.scale, {
-//           x: 1.3,
-//           y: 1.3,
-//           z: 1.3,
-//           duration: 1, // Duration of the animation in seconds
-//           ease: "power2.out", // Easing function to use
-//         });
-
-//         break;
-//       }
-
-//       targetObject = targetObject.parent;
-//     }
-//   }
-// }
-
-// window.addEventListener("click", onMouseClick, false);
-
-/**
- * Animate
- */
 const clock = new THREE.Clock();
 
-const tick = () => {
+// Shadow plane
+const planeGeometry = new THREE.PlaneGeometry(20, 20);
+const planeMesh = new THREE.Mesh(planeGeometry, shadowMaterial);
+planeMesh.receiveShadow = true;
+planeMesh.rotateX(- Math.PI / 2);
+planeMesh.position.y = -2;
+scene.add(planeMesh);
+
+function loadGLTFModel() {
+  loader.load(
+    "/cubemain2.glb",
+    function (gltf) {
+      gltfModel = gltf.scene;
+      gltfModel.traverse(function (child) {
+        if (child.isMesh) {
+          child.castShadow = true;
+        }
+      });
+      scene.add(gltf.scene);
+
+      const planes = ["Plane1", "Plane2", "Plane3", "Plane4", "Plane5"];
+
+      planes.forEach((planeName) => {
+        const plane = gltfModel.getObjectByName(planeName, true);
+
+        if (plane) {
+          plane.material = shaderMaterial;
+        }
+      });
+
+      gsap.to(gltfModel.rotation, { y: "+=" + Math.PI * 2, duration: 16, ease: "none", repeat: -1 });
+    },
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    function (error) {
+      console.log("An error happened");
+    }
+  );
+}
+
+function addLights() {
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+  scene.add(ambientLight);
+
+  const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x0000ff, 0.3);
+  scene.add(hemisphereLight);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  directionalLight.position.set(0, 2, 2);
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 0.3);
+  pointLight.position.set(2, 3, 4);
+  scene.add(pointLight);
+}
+
+function animate() {
   const elapsedTime = clock.getElapsedTime();
 
-  shaderMaterial.uniforms.u_time.value = performance.now() / 2000;
+  // stats.begin();
+  uniforms.u_time.value = elapsedTime;
 
-  pointLight.position.x = Math.sin(elapsedTime) * 2;
-  pointLight.position.y = Math.sin(elapsedTime) * 2;
-  pointLight.position.z = Math.sin(elapsedTime) * 2;
+  if (gltfModel) {
+    gltfModel.rotation.y += 0.01;
+  }
 
-  // Update controls
   controls.update();
-
-  // Render
   renderer.render(scene, camera);
 
-  // Call tick again on the next frame
+  // stats.end();
+
+  requestAnimationFrame(animate);
+}
+
+function onResize() {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+}
+
+function main() {
+  addLights();
+  loadGLTFModel();
+  animate();
+
+  window.addEventListener('resize', onResize);
+}
+
+main();
+
+// instal npm install stats.js
+
+/* 
+const tick = () => {
+  stats.begin();
+
+  const elapsedTime = clock.getElapsedTime();
+
+  // ... rest of your animation code ...
+
+  stats.end();
+
   window.requestAnimationFrame(tick);
 };
 
 tick();
+
+*/
